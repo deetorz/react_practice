e54bb6f34d0fb3cc6f30293432bb991738c372b1
Derek Torres <derek.n.torres@gmail.com>
2021-09-21 03:50:37 +0900

    added props to component

---
+++ adopt_me/src/App.js
@@ -0,0 +1,22 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, { name: "Nova", animal: "Dog", breed: "Tibetan Spaniel" }),
+      React.createElement(Pet, { name: "Katie", animal: "Dog", breed: "Maltese" }),
+      React.createElement(Pet, { name: "Hops", animal: "Rabbit", breed: "No idea" })
+    ]
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
