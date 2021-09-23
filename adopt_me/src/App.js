ceb4d6b06562eae8e4714d1e03dbd0cb5e2c4cd7
Derek Torres <derek.n.torres@gmail.com>
2021-09-23 20:15:00 +0900

    added react and eslint compatability

--- adopt_me/src/App.js
+++ adopt_me/src/App.js
@@ -1,26 +1,37 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Nova",
+//       animal: "Dog",
+//       breed: "Tibetan Spaniel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Katie",
+//       animal: "Dog",
+//       breed: "Maltese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Hops",
+//       animal: "Rabbit",
+//       breed: "No idea",
+//     }),
+//   ]);
+// };
+
+//JSX Version
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Nova",
-      animal: "Dog",
-      breed: "Tibetan Spaniel",
-    }),
-    React.createElement(Pet, {
-      name: "Katie",
-      animal: "Dog",
-      breed: "Maltese",
-    }),
-    React.createElement(Pet, {
-      name: "Hops",
-      animal: "Rabbit",
-      breed: "No idea",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Nova" animal="Dog" breed="Tiben Spaniel" />
+      <Pet name="Katie" animal="Dog" breed="Maltese" />
+      <Pet name="Hops" animal="Rabbit" breed="Shorthair" />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
