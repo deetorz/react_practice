15ca8a524a30ca3bc5b05936ff08384016cc641f
Derek Torres <derek.n.torres@gmail.com>
2021-09-24 23:42:14 +0900

    added eslint rules for react hooks

--- adopt_me/src/App.js
+++ adopt_me/src/App.js
@@ -1,35 +1,15 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Nova",
-//       animal: "Dog",
-//       breed: "Tibetan Spaniel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Katie",
-//       animal: "Dog",
-//       breed: "Maltese",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Hops",
-//       animal: "Rabbit",
-//       breed: "No idea",
-//     }),
-//   ]);
-// };
-
-//JSX Version
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Nova" animal="Dog" breed="Tiben Spaniel" />
+      <SearchParams />
+      {/* <Pet name="Nova" animal="Dog" breed="Tiben Spaniel" />
       <Pet name="Katie" animal="Dog" breed="Maltese" />
-      <Pet name="Hops" animal="Rabbit" breed="Shorthair" />
+      <Pet name="Hops" animal="Rabbit" breed="Shorthair" /> */}
     </div>
   );
 };
--- adopt_me/src/SearchParams.js
+++ adopt_me/src/SearchParams.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Tokyo, JP";
+  const [location, setLocation] = useState("Tokyo, JP");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
